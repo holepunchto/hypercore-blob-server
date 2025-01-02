1785e320a76ac6e9009eb78ac8c4472601b9f65e
natzcam <nathanielcamomot@gmail.com>
2025-01-02 17:24:07 +0800

    range tests

--- test/blobs.js
+++ test/blobs.js
@@ -1,26 +1,82 @@
 const test = require('brittle')
 const RAM = require('random-access-memory')
 const Corestore = require('corestore')
-const { testServeBlobs, request } = require('./helpers')
+const { testServeBlobs, request, testHyperblobs } = require('./helpers')
 
 test('can get blob from hypercore', async function (t) {
   const store = new Corestore(RAM)
 
-  const core = store.get({ name: 'test' })
+  const blobs = testHyperblobs(t, store)
 
-  await core.append([Buffer.from('Hello'), Buffer.from('World')])
+  const id = await blobs.put(Buffer.from('Hello World'))
 
   const serve = testServeBlobs(t, store)
   await serve.listen()
 
-  const res = await request(serve, core.key, {
-    blob: {
-      blockOffset: 0,
-      blockLength: 2,
-      byteOffset: 2,
-      byteLength: 9
-    }
-  })
+  const res = await request(serve, blobs.core.key, { blob: id })
+
   t.is(res.status, 200)
-  t.is(res.data, 'HelloWorl')
+  t.is(res.data, 'Hello World')
+})
+
+test('can get blob from hypercore - multiple blocks', async function (t) {
+  const store = new Corestore(RAM)
+
+  const blobs = testHyperblobs(t, store)
+  blobs.blockSize = 4 // force multiple blocks
+
+  const id = await blobs.put(Buffer.from('Hello World'))
+
+  const serve = testServeBlobs(t, store)
+  await serve.listen()
+
+  const res = await request(serve, blobs.core.key, { blob: id })
+
+  t.is(res.status, 200)
+  t.is(res.data, 'Hello World')
+})
+
+test('can get a partial blob from hypercore', async function (t) {
+  const store = new Corestore(RAM)
+
+  const blobs = testHyperblobs(t, store)
+
+  const id = await blobs.put(Buffer.from('Hello World'))
+
+  const serve = testServeBlobs(t, store)
+  await serve.listen()
+
+  const res = await request(serve, blobs.core.key, { blob: id, range: 'bytes=3-7' })
+  t.is(res.status, 206)
+  t.is(res.data, 'lo Wo')
+})
+
+test('can get a partial blob from hypercore, but request the whole data', async function (t) {
+  const store = new Corestore(RAM)
+
+  const blobs = testHyperblobs(t, store)
+
+  const id = await blobs.put(Buffer.from('Hello World'))
+
+  const serve = testServeBlobs(t, store)
+  await serve.listen()
+
+  const res = await request(serve, blobs.core.key, { blob: id, range: 'bytes=0-10' })
+  t.is(res.status, 206)
+  t.is(res.data, 'Hello World')
+})
+
+test('can get a partial blob from hypercore, out of range', async function (t) {
+  const store = new Corestore(RAM)
+
+  const blobs = testHyperblobs(t, store)
+
+  const id = await blobs.put(Buffer.from('Hello World'))
+
+  const serve = testServeBlobs(t, store)
+  await serve.listen()
+
+  const res = await request(serve, blobs.core.key, { blob: id, range: 'bytes=0-11' })
+  t.is(res.status, 206)
+  t.is(res.data, 'Hello World')
 })
--- test/helpers/index.js
+++ test/helpers/index.js
@@ -1,18 +1,21 @@
 const http = require('http')
 const ServeBlobs = require('../../index.js')
 const Hyperdrive = require('hyperdrive')
+const Hyperblobs = require('hyperblobs')
 
 module.exports = {
   request,
   testServeBlobs,
+  testHyperblobs,
   testHyperdrive
 }
 
-function get (link) {
+function get (link, range = null) {
   return new Promise((resolve, reject) => {
     const req = http.get(link, {
       headers: {
-        Connection: 'close'
+        Connection: 'close',
+        range
       }
     })
 
@@ -38,13 +41,20 @@ function get (link) {
 async function request (serve, key, opts) {
   const link = serve.getLink(key, opts)
 
-  return get(link)
+  return get(link, opts.range)
 }
 
 function testServeBlobs (t, store, opts) {
   const serve = new ServeBlobs(store, opts)
   t.teardown(() => serve.close())
   return serve
+}
+
+function testHyperblobs (t, store) {
+  const core = store.get({ name: 'test' })
+  const blobs = new Hyperblobs(core)
+  t.teardown(() => blobs.close())
+  return blobs
 }
 
 function testHyperdrive (t, store) {
