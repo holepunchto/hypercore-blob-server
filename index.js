bfb094e9f3247a0a4eaa7bf655afd968728275be
joker <warmachinesocial@skiff.com>
2025-03-14 17:04:36 -0500

    Added CORS

--- index.js
+++ index.js
@@ -294,6 +294,10 @@ module.exports = class HypercoreBlobServer {
   }
 
   async _onrequest (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
     if (req.method !== 'HEAD' && req.method !== 'GET') {
       req.socket.destroy()
       req.statusCode = 400
